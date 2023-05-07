9afa140c2aa135395704fc85fa9251680e5957d2
drkenni <tanyromachka1@mail.ru>
2023-05-07 19:29:39 +0300

    Add function description

--- src/Class_.py
+++ src/Class_.py
@@ -1,8 +1,5 @@
-from datetime import datetime
-
-
 class Transaction():
-
+    """Создает экземпляры с данными о транзакции"""
     def __init__(self, id_trans, date_trans, state, amount, description, to, currency, from_anyone=None):
         self.id_trans = id_trans
         self.date = date_trans
@@ -14,28 +11,21 @@ class Transaction():
         self.from_anyone = from_anyone
 
     def __repr__(self):
-        """Описание Класса"""
-        return "Класс выводящий данные на основе файла json с историей транзакций"
+        return f"Транзакция {self.id_trans}№"
 
     def __str__(self):
         return "Транзакция"
 
     def get_id(self):
-        """id операции"""
+        """id транзакции"""
         return self.id_trans
 
     def get_date(self):
+        """Дата транзакции"""
         return self.date
 
     def get_information(self):
+        """Выводит информанию на основе полученных данных о транзакции"""
         return (f"{self.date} {self.description}\n"
                 f"{self.from_anyone} -> {self.to}\n"
                 f"{self.amount} {self.currency}\n")
-
-    def chenge_date(self):
-        day_trans = datetime.fromisoformat(self.date)
-        return f"{day_trans.day}.{day_trans.month}.{day_trans.year} в {day_trans.hour}:{day_trans.minute}"
-
-
-
-
--- src/utils.py
+++ src/utils.py
@@ -4,14 +4,21 @@ from datetime import datetime
 
 
 def get_inf(json_file):
+    """Открывет json фаил и преобразует его в Python словарь
+    json_file - путь до файла
+    :return - словарь с транзакциями пользоватеря
+    """
     with open(json_file, 'r', encoding='utf-8') as f:
         trans_list = json.load(f)
         return trans_list
 
 
 def filtering_list(trans_list):
-    """ возвращает отсортированный по дате список с 5
-    последними выполненными операциями """
+    """Возвращает отсортированный по дате список с 5
+    последними выполненными операциями
+    trans_list - словарь с транзакциями пользоватеря
+    :return - список выполненых транзакций и отсортированных по дате
+    """
     # фильтр списка по "EXECUTED"
     executed_list = [item for item in trans_list if item.get('state') == 'EXECUTED']
     # сортировка по дате
@@ -20,9 +27,17 @@ def filtering_list(trans_list):
 
 
 def make_return(last_five_trans):
+    """Создает экземпляры транзакций,
+    обрабатывает данные и переводит в читаемый формат
+    last_five_trans: список выполненых транзакций и отсортированных по дате
+    :return - список с экземплярами класса Transaction
+    """
     processed_trans = []
     def check_from(where):
-        bank_account = "Вклад"
+        """Подфункция шифрующая номера карт и счетов,
+        проверяет на наличие отправителя "from"
+         """
+        bank_account = "Вклад в"
         try:
             num_wallet = operation[where]
             if num_wallet[:4] == "Счет":
@@ -32,18 +47,18 @@ def make_return(last_five_trans):
             return bank_account
         except:
             return bank_account
-
+    # создаю экземпляры класса
     for operation in last_five_trans:
 
         try:
             id_trans = operation["id"]
+            change_date = datetime.strptime(operation["date"], "%Y-%m-%dT%H:%M:%S.%f")
+            date_trans = datetime.strftime(change_date, "%d.%m.%Y")
             state = operation["state"]
-            date_full = datetime.strptime(operation["date"], "%Y-%m-%dT%H:%M:%S.%f")
-            date_trans = datetime.strftime(date_full, "%d.%m.%Y")
-            description = operation["description"]
-            from_anyone = check_from("from")
-            to = check_from("to")
             amount = operation["operationAmount"]["amount"]
+            description = operation["description"]
+            to = check_from("to")
+            from_anyone = check_from("from")
             currency = operation["operationAmount"]["currency"]["name"]
             transaction = Transaction(id_trans, date_trans, state, amount, description, to, currency, from_anyone)
             processed_trans.append(transaction)
@@ -53,5 +68,6 @@ def make_return(last_five_trans):
 
 
 def print_(operations_list):
+    """Итерируется по классам списка и выводит информацию о транзакции"""
     for operation in operations_list:
         print(operation.get_information())
